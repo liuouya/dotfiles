ff8817cf86e8d16abe46b52b3bb437097ca21495
Ouya (Anna) Liu <oliu15@bloomberg.net>
2016-10-17 19:10:36 +0100

    updated update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,17 @@ ln -si `pwd`/emacs.d/init.el ~/.emacs.d/init.el && \
     ln -si `pwd`/emacs.d/elisp ~/.emacs.d/
 
 # i3
-ln -si `pwd`/i3/config ~/.i3/config
+i3_config_1="~/.i3/"
+i3_config_2="~/.config/i3/"
+
+if [ -a $i3_config_1 ];
+then
+    echo one
+    ln -si `pwd`/i3/config $i3_config_1
+elif [ -a ~/.config/i3 ];
+then
+    echo $i3_config_2
+    ln -si `pwd`/i3/config $i3_config_2
+else
+    echo "need to init i3 config"
+fi

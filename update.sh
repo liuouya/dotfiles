14c8a5d057b0e709468392050814b57ed4ef182c
Ouya (Anna) Liu <oliu15@bloomberg.net>
2016-10-24 12:28:19 +0100

    fixed update script

--- update.sh
+++ update.sh
@@ -5,16 +5,12 @@ ln -si `pwd`/emacs.d/init.el ~/.emacs.d/init.el && \
     ln -si `pwd`/emacs.d/elisp ~/.emacs.d/
 
 # i3
-i3_config_1="~/.i3/"
-i3_config_2="~/.config/i3/"
+i3_config_1=~/.i3/
+i3_config_2=~/.config/i3/
 
-if [ -a $i3_config_1 ];
-then
-    echo one
+if [ -d $i3_config_1 ]; then
     ln -si `pwd`/i3/config $i3_config_1
-elif [ -a ~/.config/i3 ];
-then
-    echo $i3_config_2
+elif [ -d $i3_config_2 ]; then
     ln -si `pwd`/i3/config $i3_config_2
 else
     echo "need to init i3 config"
